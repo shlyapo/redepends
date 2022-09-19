42a499fc07710e0f06fbc3a946bb136cbc2ac6b4
elizabeth <shlyapoelizzz@gmail.com>
2022-09-19 23:05:45 +0300

    version 2.1. fix gz

--- apt/redepends.py
+++ apt/redepends.py
@@ -17,16 +17,25 @@ dict_deps = dict()
 
 flag = 0
 q = []
+l=0
 
 
-def procc_dep(line, dict_dep):
+def procc_dep(li, dict_d):
+    global flag
+    global q
+    global l
+    line =""
+    line = li
+    dict_dep = dict()
+    dict_dep = dict_d
     if re.search(r'Package:', line):
-        global flag
-        global q
         flag += 1
+        if l == 1:
+            q.remove(q[0])
 
         line = line[9:-1]
         q.append(line)
+
         two_deps = []
 
         if flag > 1:
@@ -36,18 +45,21 @@ def procc_dep(line, dict_dep):
         if line not in dict_dep:
             dict_dep[line] = two_deps
 
+
     if re.search(r'Depends:', line) or re.search(r'Provides:', line):
         if re.search(r'Pre-Depends:', line):
             return dict_dep
-
+        l+=1
         flag = 0
-        line = line[9:-1]
+        #line = line[9:-1]
+        line = re.sub(r'Depends: ', '', line)
+        line = re.sub(r'Provides: ', '', line)
         line = re.sub(r' \([^)]*\)', '', line)
         line = re.sub(r',', '', line)
         line = re.sub(r'\|', '', line)
         line = re.sub(r':any', '', line)
+
         s = q[0]
-        q.remove(q[0])
         res = re.split(' +', line)
 
         for len in res:
@@ -60,6 +72,9 @@ def procc_dep(line, dict_dep):
                 b.append(s)
                 dict_dep[len] = b
 
+        if l == 2:
+            q.remove(q[0])
+
     return dict_dep
 
 
@@ -68,10 +83,9 @@ def dep_list(content):
     list_dir = []
 
     if os.path.exists(content):
-        for address, dirs, folders in os.walk(content):
-            if dir_name in folders:
-                p = os.path.join(address, dir_name)
-                list_dir.append(p)
+        for rootdir, dirs, files in os.walk(content):
+            if (rootdir.split('/')[-1]) == dir_name:
+                list_dir.append(rootdir)
 
         while True:
             print("Choose dir, that you need")
@@ -84,20 +98,22 @@ def dep_list(content):
             ans = input()
             if ans.isdigit():
                 ans = int(ans)
-                if ans <= count:
-                    content = list_dir[ans]
+                if 0 < ans <= count:
+                    content = list_dir[ans-1]
                     break
                 else:
                     print("Wrong input")
             else:
                 print("Wrong input")
 
-        for address, dirs, filenames in os.walk(content):
-            for file in filenames:
-                a = re.split(".", file)
-                if a[0] == "Packages":
-                    content = content + file
+
+        for rootdir, dirs, files in os.walk(content):
+            for file in files:
+                if (file.split('.')[0]) == 'Packages':
+                    content = os.path.join(content, file)
                     break
+
+        pa = content.split('/')[-1]
 
         if tarfile.is_tarfile(content):
             with tarfile.open(content, "r") as tar:
@@ -116,6 +132,17 @@ def dep_list(content):
                         line = line[2:-1]
                         d = procc_dep(line, d)
                     return d
+
+        elif pa.split(".")[-1] == 'gz':
+            dict_de = dict()
+            with gzip.open(content, 'rb') as f:
+                with io.TextIOWrapper(f, encoding='utf-8') as decoder:
+                    f = decoder.readlines()
+                    for line in f:
+                        da = procc_dep(line, dict_de)
+                        dict_de = da
+                    return dict_de
+
         else:
             with io.TextIOWrapper(content, encoding='utf-8') as decoder:
                 f = decoder.readlines()
@@ -133,26 +160,27 @@ def dfs(visited, graph, node, step):
         print(step + node)
         visited.add(node)
         for neighbor in graph[node]:
+            if neighbor is graph[node][0]:
+                step = step + '.'
             dfs(visited, graph, neighbor, step)
-            if neighbor is graph[-1]:
-                step = step + "."
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Redepends', prog='redepends')
     parser.add_argument('-p', type=str, help='Name of package', nargs="+")
-    parser.add_argument('-h', action='store_const', const=True,
+    parser.add_argument('-u',
                         help='Add this arquments and url')
-    parser.add_argument('-d', action='store_const', const=True, help='Input local dir for repository')
+    parser.add_argument('-d', help='Input local dir for repository')
     args = parser.parse_args()
 
-    if args.d and args.h:
+    if args.d is not None and args.u is not None:
          print("Wrong input")
 
-    elif args.h:
+    elif args.u is not  None: # think
 
         try:
-            r = requests.get(args.d)
+            r = requests.get(args.u)
             r.raise_for_status()
             if tarfile.is_tarfile(r.content):
                 with open('pack.tar', 'wb') as f:  # подумать
@@ -183,10 +211,9 @@ if __name__ == '__main__':
         except requests.exceptions.HTTPError as err:
             raise SystemExit(err)
 
-    elif args.d:
+    elif args.d is not None:
 
         dir = args.d  # "/dists/stable/main/binary-amd64/Packages.gz"
-        print(dir)
         dd = dep_list(dir)
 
     else:
